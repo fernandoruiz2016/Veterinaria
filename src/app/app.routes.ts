7cc1c62a5c56ab65a5c4763fb7ccbf950e553ad3
Fernando Ruiz <fernandoruiz696@gmail.com>
2026-03-03 20:22:06 -0500

    Historial de mascotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { EditarCita } from './features/citas/pages/editar-cita/editar-cita';
 import { MascotasComponent } from './features/mascotas/pages/mascotas-component/mascotas-component';
 import { CrearMascota } from './features/mascotas/pages/crear-mascota/crear-mascota';
 import { EditarMascota } from './features/mascotas/pages/editar-mascota/editar-mascota';
+import { VerHistorial } from './features/mascotas/pages/ver-historial/ver-historial';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,7 @@ export const routes: Routes = [
             { path: 'mascotas', component: MascotasComponent },
             { path: 'mascotas/crear', component: CrearMascota },
             { path: 'mascotas/editar/:id', component: EditarMascota },
+            { path: 'mascotas/historial/:id', component: VerHistorial },
 
             { path: 'citas', component: CitasComponent },
             { path: 'citas/crear', component: CrearCita },
