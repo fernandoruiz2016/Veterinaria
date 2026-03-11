0ef6e423901be0adff94daf1fbd9b606d0c6eb0a
alexrmcr777 <alexrmcr@gmail.com>
2026-03-10 23:03:20 -0500

    Delete src directory

--- src/app/models/citas.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Cita {
-    id_cita?: number;      // Opcional para nuevas citas
-    mascota: string;
-    veterinario: string;
-    fecha: string;         // Formato 'YYYY-MM-DD'
-    hora: string;          // Formato 'HH:mm'
-    estado: 'Programada' | 'Atendida' | 'Cancelada' | 'No asistió'; // Estados definidos
-    comentarios?: string;
-}
--- src/app/models/mascotas.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Mascota {
-    id_mascota?: number;
-    nombre: string;
-    dueno: string;
-    telefono: string;
-    email: string;
-}
