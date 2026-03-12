f55854905c3b24e6e16d896f78ec16b1dec5e888
alexrmcr777 <alexrmcr@gmail.com>
2026-03-12 10:34:44 -0500

    Update nav-bar.ts

--- src/app/shared/components/nav-bar/nav-bar.ts
+++ src/app/shared/components/nav-bar/nav-bar.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterModule],
+  imports: [RouterModule, AsyncPipe],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
 export class NavBar {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  get isLoggedIn$() {
+    return this.authService.isLoggedIn$;
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
