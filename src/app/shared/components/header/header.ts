127e4e03997dba0fb82fc9804eb4aba4e584ee1f
alexrmcr777 <alexrmcr@gmail.com>
2026-03-12 10:36:30 -0500

    Update header.ts
    Added AsyncPipe import and updated the Header component to include router dependency and logout functionality.

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, AsyncPipe],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   isOpen = false;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  get isLoggedIn$() {
+    return this.authService.isLoggedIn$;
+  }
+
   toggleMenu() {
     this.isOpen = !this.isOpen;
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
 }
