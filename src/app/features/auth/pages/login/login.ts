b779522e6aa44fdfbfccc8dc705b0a4dd4fc40d7
alexrmcr777 <alexrmcr@gmail.com>
2026-03-12 10:31:33 -0500

    Add files via upload

---
+++ src/app/features/auth/pages/login/login.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../../services/auth';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  usuario = '';
+  password = '';
+  error = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit(): void {
+    this.error = '';
+
+    const ok = this.authService.login(this.usuario.trim(), this.password.trim());
+
+    if (!ok) {
+      this.error = 'Ingresa usuario y contraseña válidos.';
+      return;
+    }
+
+    this.router.navigateByUrl('/');
+  }
+}
+
